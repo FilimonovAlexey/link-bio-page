e86d9424245d1d5eb78caaf04c34c2d2c6a53a04
Alexey Filimonov <alexeyf08@gmail.com>
2023-09-16 21:09:31 +0700

    Соцсети вынесены в отдельную переменную

--- src/App.js
+++ src/App.js
@@ -12,9 +12,18 @@ import {
   List,
   HStack
 } from '@chakra-ui/react';
-import {AiFillCode} from 'react-icons/ai'
+import {AiFillCode, AiFillGithub} from 'react-icons/ai';
+import { FaTelegram } from "react-icons/fa"
 
 function App() {
+
+  //Ссылки на социальные сети и их логотипы
+  const links = [
+    {url: "https://filimonov-blog.ru/", icon: AiFillCode, text: "Мой Сайт"},
+    {url: "https://github.com/FilimonovAlexey", icon: AiFillGithub, text: "Мой GitHub"},
+    {url: "https://t.me/tehnomaniak07", icon: FaTelegram, text: "Telegram група"},
+  ]
+
   return (
     <ChakraProvider theme={theme}>
       <Box textAlign="center" fontSize="xl">
@@ -30,23 +39,31 @@ function App() {
               Техноманьяк
             </Text>
             <List>
-              <a href='https://filimonov-blog.ru/'>
-                <HStack 
-                  w="15em" 
-                  h="3em" 
-                  borderRadius="lg" 
-                  bgColor="rgba(255,255,255,0.7)"
-                  p="1em"
+              {links.map(link => (
+                <a href={link.url}>
+                  <HStack 
+                    w="15em" 
+                    h="3em" 
+                    borderRadius="lg" 
+                    bgColor="rgba(255,255,255,0.7)"
+                    p="1em"
+                    marginY="1em"
                   >
                     <Text 
-                      as={AiFillCode} 
+                      as={link.icon} 
                       color="gray.900" 
                       fontSize={30} 
                       marginRight="auto"
                     />
-                    <Text color="gray.900" fontSize={20} fontWeight="bold">Мой сайт</Text>
-                </HStack>
-              </a>
+                    <Text 
+                      color="gray.900"
+                      fontSize={20} 
+                      fontWeight="bold">
+                        {link.text}
+                    </Text>
+                  </HStack>
+                </a>
+              ))}
             </List>
           </VStack>
         </Grid>
