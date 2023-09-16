af72836274b503f1e7fb4e1502f927d8157dc0cc
Alexey Filimonov <alexeyf08@gmail.com>
2023-09-16 18:43:52 +0700

    добавлен ссылка

--- src/App.js
+++ src/App.js
@@ -8,16 +8,37 @@ import {
   Code,
   Grid,
   theme,
-  Image
+  Image,
+  List,
+  HStack
 } from '@chakra-ui/react';
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
       <Box textAlign="center" fontSize="xl">
-        <Grid minH="100vh" p={3}>
+        <Grid minH="100vh" p={3} bgGradient="linear(to-b, gray.900, blue.200)">
           <VStack>
-            <Image src="./Logo512.png"/>
+            <Image 
+              src="./Logo512.png"
+              w="10em"
+              borderRadius="50%"
+              boxShadow="0px 0px 30px rgba(0,0,0,0.5)"
+            />
+            <Text as="h1" fontSize={30} fontWeight="bold">
+              Техноманьяк
+            </Text>
+            <List>
+              <a href='https://filimonov-blog.ru/'>
+                <HStack 
+                  w="15em" 
+                  h="3em" 
+                  borderRadius="lg" 
+                  bgColor="rgba(255,255,255,0.7)">
+                    <Text color="gray.900" fontSize={20} fontWeight="bold">Мой сайт</Text>
+                </HStack>
+              </a>
+            </List>
           </VStack>
         </Grid>
       </Box>
