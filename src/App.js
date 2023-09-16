03ffa224273d47ecfdb78402414e62877bb88dd0
Alexey Filimonov <alexeyf08@gmail.com>
2023-09-16 18:19:39 +0700

    Убрано все лишнее

--- src/App.js
+++ src/App.js
@@ -8,30 +8,16 @@ import {
   Code,
   Grid,
   theme,
+  Image
 } from '@chakra-ui/react';
-import { ColorModeSwitcher } from './ColorModeSwitcher';
-import { Logo } from './Logo';
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
       <Box textAlign="center" fontSize="xl">
         <Grid minH="100vh" p={3}>
-          <ColorModeSwitcher justifySelf="flex-end" />
-          <VStack spacing={8}>
-            <Logo h="40vmin" pointerEvents="none" />
-            <Text>
-              Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
-            </Text>
-            <Link
-              color="teal.500"
-              href="https://chakra-ui.com"
-              fontSize="2xl"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn Chakra
-            </Link>
+          <VStack>
+            <Image src="./Logo512.png"/>
           </VStack>
         </Grid>
       </Box>
