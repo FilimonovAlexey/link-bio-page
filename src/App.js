4c5992a404ea228674a3a692a1e4849366d49706
Alexey Filimonov <alexeyf08@gmail.com>
2023-09-28 23:24:28 +0700

    обновлены иконки

--- src/App.js
+++ src/App.js
@@ -27,6 +27,7 @@ import {FaTelegram,
   FaLinkedin, 
   FaVk, 
   FaGlobe,
+  FaYoutube
 } from "react-icons/fa"
 
 //Подключение анимаций
@@ -47,8 +48,8 @@ function App() {
     const devlinks = [
       {url: "https://github.com/FilimonovAlexey/awesome-frontend-dev", icon: AiFillGithub, text: "Полезные материалы для Программиста"},
       {url: "https://github.com/FilimonovAlexey/school21", icon: AiFillGithub, text: "Подготовка к Школе 21"},
-      {url: "https://youtu.be/3G1-r8uqbnc?si=Vd67S5T9zDGR9Xc9", icon: AiFillCustomerService, text: "Настрой свой VPN"},
-      {url: "https://youtu.be/2K69fwmCGQc?si=g__lQ-WLCZ00eapF", icon: AiFillCustomerService, text: "Telegram бот с chatGPT"},
+      {url: "https://youtu.be/3G1-r8uqbnc?si=Vd67S5T9zDGR9Xc9", icon: FaYoutube, text: "Настрой свой VPN"},
+      {url: "https://youtu.be/2K69fwmCGQc?si=g__lQ-WLCZ00eapF", icon: FaYoutube, text: "Telegram бот с chatGPT"},
     ]
 
     //Название страницы
