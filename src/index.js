676e6a58cc988e6e88792e8b685edffe04dac797
Alexey <alexeyf08@gmail.com>
2024-06-26 19:41:34 +0700

    Измерение скорости сайта Vercel

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import { StrictMode } from 'react';
 import { ColorModeScript } from "@chakra-ui/react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import App from "./App";
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 ReactDOM.render(
   <StrictMode>
     <ColorModeScript />
+    <SpeedInsights/>
     <Router>
       <Routes>
         <Route path="/" element={<App />} />
